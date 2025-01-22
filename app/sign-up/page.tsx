d6ab331cfa3ea848251a77307746a488a232fe44
mahnoor adnan <mahnooradnan479@gmail.com>
2025-01-23 00:09:15 +0500

    changes

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import React from 'react';
-import { useState } from 'react';
 import Navbar from '@/components/Navbar';
 import Header from '@/components/Header';
 import Image from 'next/image';
